ed2eee59090851a261cecb905a06e5f788f56fee
Chase02 <2325190228@qq.com>
2018-04-08 16:03:22 +0800

    test第三提交

--- src/com/wucb/stu/control/UserControl.java
+++ src/com/wucb/stu/control/UserControl.java
@@ -6,6 +6,10 @@ public class UserControl {
 		System.out.println("getuser");
 	}
 	
+	public void test1(){
+		System.out.println("text-");
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("学习测试中");
 	}
